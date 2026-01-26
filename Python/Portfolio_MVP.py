d885506131679084949b4219e50d102631bad9f6
Renan Pacheco <rpstrabalho@gmail.com>
2026-01-26 12:26:49 -0300

    incluido mais 2 analises simples e duas novas colunas ao dataset.

--- Python/Portfolio_MVP.py
+++ Python/Portfolio_MVP.py
@@ -81,8 +81,11 @@ ganho_por_faixa = (
     .sum()
 )
 ordem = ['Madrugada', 'Manhã', 'Tarde', 'Noite']
+
 ganho_por_faixa['faixa_horaria'] = pd.Categorical(ganho_por_faixa['faixa_horaria'], categories=ordem, ordered=True)
+
 ganho_por_faixa = ganho_por_faixa.sort_values('faixa_horaria')
+
 plt.figure(figsize=(8,5))
 sns.barplot(x='faixa_horaria', y='ganho_total', data=ganho_por_faixa, palette='magma')
 plt.title('Ganho Total por Faixa Horária')
@@ -92,4 +95,82 @@ plt.show()
 
 ## Aqui, foi preciso lidar com o formato da coluna 'horario' que estava como string, convertendo-a para datetime para extrair a hora corretamente. 
 
-## Análise 3: Análise de séries temporais - Vamos analisar a tendência do ganho total ao longo do tempo
+## Análise 3: Ganhos por Quilometragem - Vamos analisar o ganho total por faixa de quilometragem percorrida. Primeiro, criaremos faixas de quilometragem e depois somaremos os ganhos em cada faixa.
+## A coluna distancia_corridA(km) tem o status : cancelado e por isso, está com o formato object, vamos converter para numérico, transformando os valores ' cancelado ' em NaN
+df_merged['distancia_corrida(km)'] = pd.to_numeric(df_merged['distancia_corrida(km)'], errors='coerce')
+df_merged['distancia_embarque(km)'] = pd.to_numeric(df_merged['distancia_embarque(km)'], errors='coerce')
+## validando a transformação : 
+# print(df_merged[['distancia_corrida(km)', 'distancia_embarque(km)']].dtypes)
+
+## Quantidade total de cancelamentos:
+total_cancelamentos = df_merged['distancia_corrida(km)'].isna().sum()
+print(f'Total de cancelamentos: {total_cancelamentos}')
+
+## Criando a coluna de km total
+df_merged['km_total'] = df_merged['distancia_corrida(km)'].fillna(0) + df_merged['distancia_embarque(km)'].fillna(0)
+df_merged['ganho_sem_bonus'] = df_merged['pg_motorista'].fillna(0)
+
+## Vamos analisar o ganho por km rodado, incluindo os ganhos de preco_dinamico e turbo
+df_merged['ganho_km_valor_total'] = df_merged['valor_total'] / df_merged['km_total'].replace(0 , np.nan)
+
+## Vamos analisar o ganho por km rodado, apenas com o valor base do motorista entitulado como 'pg_motorista'
+df_merged['ganho_km_base'] = df_merged['ganho_sem_bonus'] / df_merged['km_total'].replace(0 , np.nan)
+
+## Criando faixas de quilometragem
+bins = [ 0, 2, 5, 10, 15, 20, 30, 50]
+
+labels = ['0-2 km', '2-5 km', '5-10 km', '10-15 km', '15-20 km', '20-30 km', '30-50 km']
+
+df_merged['faixa_km'] = pd.cut(df_merged['km_total'], bins=bins, labels=labels, right=False)
+
+analise_km = df_merged.groupby('faixa_km').agg({
+    'ganho_km_valor_total': 'mean',
+    'ganho_km_base': 'mean',
+    'km_total': 'count'
+})
+
+analise_km.rename(columns={'km_total': 'quantidade_corridas'}, inplace=True)
+
+## Plotando os resultados
+x = np.arange(len(analise_km))
+largura = 0.35
+
+plt.figure()
+plt.bar( x - largura/2, analise_km['ganho_km_valor_total'], width=largura, label='Ganho por km (Valor Total)', color='b')
+
+plt.bar( x + largura/2, analise_km['ganho_km_base'], width=largura, label='Ganho por km (Base)', color='g')
+
+plt.xticks(x, analise_km.index)
+plt.xlabel('Faixa de KM')
+plt.ylabel('Ganho Médio por KM (R$)')
+plt.title('Comparação: Ganho por KM - Valor Total vs Base')
+plt.legend()
+plt.tight_layout()
+plt.show()
+
+## Análise 4: Ganho por hora - Vamos analisar o ganho total por hora do dia, para isso, precisamos incluir uma informação que não há nos dados que é tempo de deslocamento para cada corrida. Nessa situação , o valor base será de 6 minutos, que é o tempo médio de deslocamento para cada corrida levando em consideração fatores como : engarrafamentos, vias locais, semáforos, etc.
+df_merged['tempo_deslocamento_min'] = 6
+df_merged['duracao_total_min'] = df_merged['tempo_deslocamento_min'] + df_merged['duracao_min'].fillna(0)
+df_merged['duracao_total_hora'] = df_merged['duracao_total_min'] / 60
+df_merged['ganho_hora'] = df_merged['valor_total'] / df_merged['duracao_total_hora'].replace(0 , np.nan)
+
+analise_hora = df_merged.groupby('faixa_horaria').agg({
+    'valor_total': 'sum',
+    'duracao_total_hora': 'sum'
+})
+
+analise_hora['ganho_hora_real'] = (analise_hora['valor_total'] / analise_hora['duracao_total_hora']).replace(0 , np.nan)
+analise_hora = analise_hora.reindex(ordem)
+
+plt.figure()
+plt.bar(
+    analise_hora.index,
+    analise_hora['ganho_hora_real']
+)
+
+plt.xlabel('Faixa Horária')
+plt.ylabel('Ganho por Hora (R$)')
+plt.title('Ganho por Hora Real por Faixa Horária')
+plt.show()
+
+## Análise 5:
